3a2f69acb4cfedaf7528535c122bd33807846b92
Marina Kukso <marina.kukso@gmail.com>
2019-08-26 13:41:33 -1000

    can generate denormalized records from pbf extract

---
+++ example/generate-denormalized-records.js
@@ -0,0 +1,35 @@
+//input osm data put it in a through stream, like the pbf parser.
+//denormalize data
+//pipe it out.
+var fs = require('fs')
+var through = require('through2')
+var parseOSM = require('osm-pbf-parser')
+ 
+var osm = parseOSM()
+var denormalizedItems = {}
+var references = {}
+var itemRefArray
+
+fs.createReadStream(process.argv[2])
+  .pipe(osm)
+  .pipe(through.obj(write, end))
+
+function write (items, enc, next) {
+  items.forEach(function (item) {
+    if (item.type === 'node') {
+      references[item.id] = [item.lat, item.lon]
+    }
+    if (item.type === 'way') {
+      denormalizedItems[item.id] = item
+      itemRefArray = item.refs.map(function(ref){
+        return references[ref]  
+      })
+      denormalizedItems[item.id].refs = itemRefArray
+    }
+  })
+  next()
+}
+
+function end (next) {
+  console.log(denormalizedItems)
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+1 denormalized record is input
+output is buffer for 1 record
+*/
+
+module.exports = function (denormalizedRecord) {
+
+}
