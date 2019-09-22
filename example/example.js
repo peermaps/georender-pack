bfd707d64400ea720cb1d90eadf8ed05ce78ee7d
Marina Kukso <marina.kukso@gmail.com>
2019-09-21 16:45:45 -1000

    reworked api: module takes an item and items refs object as arguments

--- example/example.js
+++ example/example.js
@@ -7,7 +7,8 @@ var parseOSM = require('osm-pbf-parser')
 var georenderPack = require('../index.js')
  
 var osm = parseOSM()
-var denormalizedItems = {}
+var allItems = {}
+var itemsRefsObject = {}
 
 fs.createReadStream(process.argv[2])
   .pipe(osm)
@@ -16,19 +17,22 @@ fs.createReadStream(process.argv[2])
 function write (items, enc, next) {
   items.forEach(function (item) {
     if (item.type === 'node') {
-      denormalizedItems[item.id] = item
+      allItems[item.id] = item
     }
     else if (item.type === 'way') {
-      denormalizedItems[item.id] = item
-      denormalizedItems[item.id].refs = item.refs.map(function(ref){
-        return [denormalizedItems[ref].lon, denormalizedItems[ref].lat]  
+      allItems[item.id] = item
+      item.refs.forEach(function (ref) {
+        if (!itemsRefsObject[ref]) itemsRefsObject[ref] = allItems[ref]
+        else return
       })
     }
   })
   next()
 }
 function end (next) {
-  Object.values(denormalizedItems).forEach(function(entry){
-    if (entry.type === 'node') console.log(entry, georenderPack(entry))
+  //object.keys..do georenderPack on each one of those. 
+  //georenderPack(entry, refsObject)
+  Object.values(allItems).forEach(function (item) {
+    console.log(georenderPack(item, itemsRefsObject))
   })
 }
--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 output is buffer for 1 record
 */
 var features = require('./features.json')
+var isArea = require('../osm-is-area/main.js')
 
-module.exports = function (denormalizedRecord) {
+module.exports = function (item, itemRefsObject) {
   var buf = Buffer.alloc(25)
   var type
-  if (Object.keys(denormalizedRecord.tags).length !== 0){
-    var tags = Object.entries(denormalizedRecord.tags)
+  if (Object.keys(item.tags).length !== 0){
+    var tags = Object.entries(item.tags)
     tags.forEach(function (tagPair) {
       if (features[tagPair[0] + '.' + tagPair[1]]){
         type = features[tagPair[0] + '.' + tagPair[1]]
@@ -17,12 +18,20 @@ module.exports = function (denormalizedRecord) {
     })
   }
   else type = 277 //place.other
-  if (denormalizedRecord.type === 'node') {
+  if (item.type === 'node') {
     buf.writeUInt8(0x01, 0) 
     buf.writeUInt32LE(type, 1) //type
-    buf.writeDoubleLE(denormalizedRecord.id, 5)
-    buf.writeFloatLE(denormalizedRecord.lon, 13)
-    buf.writeFloatLE(denormalizedRecord.lat, 17)
+    buf.writeDoubleLE(item.id, 5)
+    buf.writeFloatLE(item.lon, 13)
+    buf.writeFloatLE(item.lat, 17)
+  }
+  if (item.type === 'way') {
+    buf.writeUInt8(0x02, 0)
+    buf.writeUInt32LE(type, 1) //type
+    buf.writeDoubleLE(item.id, 5)
+    buf.writeUInt16LE(item.refs.length, 13)
+    //buf.writeFloatLE(item.lon, 15)
+    //buf.writeFloatLE(item.lat, 17)
   }
   return buf
 }
