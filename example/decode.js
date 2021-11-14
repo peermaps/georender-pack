1d759da4adc2b06c9eef854e1de64add88bf22e7
marina <marina.kukso@gmail.com>
2021-11-13 17:09:22 -1000

    updated example and readme

--- example/decode.js
+++ example/decode.js
@@ -1,9 +1,9 @@
 var decode = require('../decode.js')
 
 var buffers = [
-  Buffer.from('039502ed8f9330046c63ef412932f841bd68ef418534f8419f6bef41b731f8416c63ef412932f84101010302000000000000000000', 'hex'),
-  Buffer.from('019502a3fdb0bb0e53a5f041b158f941', 'hex'),
-  Buffer.from('02c801a09bd3b701022154f141df2ef941c054f141132ff941', 'hex')
+  Buffer.from('029b038c948b7004b17cef41a98ef941b47def411490f941a77eef413391f941f97eef418f91f94100', 'hex'),
+  Buffer.from('029b038f948b7003f95fef418190f941c95fef412d90f941955fef41d28ff94100', 'hex'),
+  Buffer.from('01f105f0b48997015bc0ef415dfaf941163dd985d8b2d984d982d8a7d98620d8abd8b1d988d8aa00', 'hex')
 ]
 
 console.log(decode(buffers))
